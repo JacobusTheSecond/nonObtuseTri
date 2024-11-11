ca746b8b57b474a51138873e5c735fee16e67921
Jacobus Conradi <conradi@cs.uni-bonn.de>
2024-11-11 14:05:30 +0100

    removed solutions in favor of summaries

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,12 +1,13 @@
 import logging
 import os
+import pickle
 
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("TkAgg")
 import numpy as np
 from pathlib import Path
-from cgshop2025_pyutils import InstanceDatabase,ZipSolutionIterator,verify
+from cgshop2025_pyutils import InstanceDatabase, ZipSolutionIterator, verify, ZipWriter
 
 from hacky_internal_visualization_stuff import plot_solution
 
@@ -76,37 +77,95 @@ def updatePlot(ax1,ax2,ax3,diff,diffHeat,zippedList,idb,name,baseName):
     plot_solution(ax3, instance, sol2, result2,prefix=name[plot_counter])
     ax1.set_title(instance.instance_uid)
 
+def loadSolutions(foldername):
+
+    best = []
+    #first unpickle
+    fn = foldername.name
+    summaryName = foldername.parent.parent/"solution_summaries"/(str(fn)+".zip")
+    pickelName = foldername.parent.parent/"solution_summaries"/(str(fn)+".pkl")
+    if summaryName.exists():
+        i = 0
+        names = pickle.load(open(pickelName, "rb"))
+
+        for sol in ZipSolutionIterator(summaryName):
+            if len(best) == i:
+                best.append([sol,names[i]])
+            elif len(sol.steiner_points_x) < len(best[i][0].steiner_points_x):
+                assert sol.instance_uid == best[i][0].instance_uid
+                best[i] = [sol,names[i]]
+                logging.info(str(names[i])+" is better at "+str(sol.instance_uid) +" with solution size "+str(len(sol.steiner_points_x)))
+            i += 1
+
+    #first build the list
+    if foldername.exists():
+        for solname in foldername.iterdir():
+            i = 0
+            logging.info("reading "+str(solname))
+            for sol in ZipSolutionIterator(solname):
+                if len(best) == i:
+                    best.append([sol,solname])
+                elif len(sol.steiner_points_x) < len(best[i][0].steiner_points_x):
+                    assert sol.instance_uid == best[i][0].instance_uid
+                    best[i] = [sol,solname]
+                    logging.info(str(solname)+" is better at "+str(sol.instance_uid) +" with solution size "+str(len(sol.steiner_points_x)))
+                i += 1
+
+    if len(best) == 0:
+        logging.error("No matching data found for folder "+str(foldername))
+        return []
+
+    #now rebuild summary
+    if summaryName.exists():
+        summaryName.unlink()
+
+    if pickelName.exists():
+        pickelName.unlink()
+    #Write the solutions to a new zip file
+    toWriteNames = []
+    with ZipWriter(summaryName) as zw:
+        logging.info("writting sol summary at "+str(summaryName))
+        for solution,name in best:
+            zw.add_solution(solution)
+            toWriteNames.append(name)
+    with open(pickelName, "wb") as f:
+        logging.info("writting name summary at "+str(pickelName))
+        pickle.dump(toWriteNames, f)
+
+    return best
+
+
 def compareSolutions(base,others):
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%H:%M:%S", level=logging.INFO)
 
     filepath = Path(__file__)
     idb = InstanceDatabase(filepath.parent.parent/"challenge_instances_cgshop25"/"zips"/"challenge_instances_cgshop25_rev1.zip")
 
-    #sols1 = ZipSolutionIterator(zips/base)
-    basesols = []
+    bestBases = []
     for name in base:
-        logging.info("reading " + str(name))
-        basesols.append([sol for sol in ZipSolutionIterator(name)])
+        best = loadSolutions(name)
+        i = 0
+        for sol,solname in best:
+            if len(bestBases) == i:
+                bestBases.append([sol,solname])
+            elif len(sol.steiner_points_x) < len(bestBases[i][0].steiner_points_x):
+                assert sol.instance_uid == bestBases[i][0].instance_uid
+                bestBases[i] = [sol,solname]
+                logging.info(str(solname)+" is better at "+str(sol.instance_uid) +" with solution size "+str(len(sol.steiner_points_x)))
+            i += 1
 
-    transformedOtherSols = [[] for sol in basesols[0]]
-    #transform
-    for i in range(len(basesols)):
-        for j in range(len(basesols[0])):
-            transformedOtherSols[j].append(basesols[i][j])
-    basesols = transformedOtherSols
-
-
-    othersols = []
+    bestOthers = []
     for name in others:
-        logging.info("reading " + str(name))
-        othersols.append([sol for sol in ZipSolutionIterator(name)])
-
-    transformedOtherSols = [[] for sol in othersols[0]]
-    #transform
-    for i in range(len(othersols)):
-        for j in range(len(othersols[0])):
-            transformedOtherSols[j].append(othersols[i][j])
-    othersols = transformedOtherSols
+        best = loadSolutions(name)
+        i = 0
+        for sol,solname in best:
+            if len(bestOthers) == i:
+                bestOthers.append([sol,solname])
+            elif len(sol.steiner_points_x) < len(bestOthers[i][0].steiner_points_x):
+                assert sol.instance_uid == bestOthers[i][0].instance_uid
+                bestOthers[i] = [sol,solname]
+                logging.info(str(solname)+" is better at "+str(sol.instance_uid) +" with solution size "+str(len(sol.steiner_points_x)))
+            i += 1
 
     global plot_counter
     plot_counter = 0
@@ -116,18 +175,18 @@ def compareSolutions(base,others):
     name = []
     fullList = []
 
-    for bases,other in zip(basesols,othersols):
-        logging.info("identifying best for " + str(bases[0].instance_uid))
-        baseIdx = np.argmin([len(a.steiner_points_x) for a in bases])
-        a = bases[baseIdx]
-        idx = np.argmin([len(o.steiner_points_x) for o in other])
+    for base,other in zip(bestBases,bestOthers):
+        #logging.info("identifying best for " + str(base[0].instance_uid))
+        a = base[0]
+        b = other[0]
+        #idx = np.argmin([len(o.steiner_points_x) for o in other])
         #other = b if len(b.steiner_points_x) < len(c.steiner_points_x) else c
-        assert a.instance_uid == other[idx].instance_uid
-        zippedList.append([a,other[idx]])
-        diff.append(len(a.steiner_points_x) - len(other[idx].steiner_points_x))
+        assert a.instance_uid == b.instance_uid
+        zippedList.append([a,b])
+        diff.append(len(a.steiner_points_x) - len(b.steiner_points_x))
         #baseName.append(bases[baseIdx])
         #name.append(others[idx])
-        fullList.append([[a,other[idx]],len(a.steiner_points_x),len(a.steiner_points_x) - len(other[idx].steiner_points_x),others[idx].name,base[baseIdx].name,idb[a.instance_uid].num_points])
+        fullList.append([[a,b],len(a.steiner_points_x),len(a.steiner_points_x) - len(b.steiner_points_x),str(other[1].parent.name)+"/"+str(other[1].name),str(base[1].parent.name)+"/"+str(base[1].name),idb[a.instance_uid].num_points])
 
     #fullList = sorted(fullList,key = lambda entry : str(entry[0][0].instance_uid))
     #fullList = sorted(fullList,key = lambda entry : entry[5])
@@ -207,12 +266,11 @@ if __name__=="__main__":
     withComplicatedCenter = filepath.parent.parent/"instance_solutions"/"withComplicatedCenter"
     withConstrainedVoronoi = filepath.parent.parent/"instance_solutions"/"withConstrainedVoronoi"
     new1 = filepath.parent.parent/"instance_solutions"/"constrainedVoronoiFromInside"
-    #new2 = filepath.parent.parent/"instance_solutions"/"constrainedVoronoiFromOutside"
+    new2 = filepath.parent.parent/"instance_solutions"/"constrainedVoronoiFromOutside"
 
-    allexceptnumeric = []
-    for list in [exact_solutions,new,seeded,seededEndFace,seededFace,withFace,withComplicatedCenter,output,gigaSeeded,withConstrainedVoronoi]:
-        allexceptnumeric = allexceptnumeric + [v for v in list.iterdir()]
+    allexceptnumeric = [exact_solutions,new,seeded,seededEndFace,seededFace,withFace,withComplicatedCenter,output,gigaSeeded,withConstrainedVoronoi]
+        #allexceptnumeric = allexceptnumeric + [v for v in list.iterdir()]
 
     #compareSolutions(base=[v for v in seeded.iterdir() if len([w for w in out.iterdir() if v.name == w.name])>0],others=[v for v in out.iterdir()])
-    compareSolutions(others=[v for v in new1.iterdir()],base=allexceptnumeric)
+    compareSolutions(others=[new1,new2],base=allexceptnumeric)
     #compareSolutions(base=[v for v in seeded.iterdir()],others=[v for v in out.iterdir()])
