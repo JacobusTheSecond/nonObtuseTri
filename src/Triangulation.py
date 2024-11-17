8cb75ce80b0fea71310ef13ec3eb456a692e0a17
Jacobus Conradi <conradi@cs.uni-bonn.de>
2024-11-17 23:21:19 +0100

    small fix

--- src/Triangulation.py
+++ src/Triangulation.py
@@ -5,7 +5,6 @@ import matplotlib.ticker
 import matplotlib.pyplot as plt
 from cgshop2025_pyutils import Cgshop2025Solution, Cgshop2025Instance
 from cgshop2025_pyutils.geometry import FieldNumber, Point, Segment
-from sqlalchemy.sql.operators import truediv
 
 import exact_geometry as eg
 from constants import *
