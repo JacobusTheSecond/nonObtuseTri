0f355b39c74b2673fccef7e1983c4c5b6803a701
Jacobus Conradi <conradi@cs.uni-bonn.de>
2024-11-19 01:08:50 +0100

    new paramterset

--- src/main.py
+++ src/main.py
@@ -178,7 +178,7 @@ def seeded_Multi():
     np.set_printoptions(linewidth=4*(96//2)+3,formatter={"all":lambda x: str(x).rjust(3)})
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%H:%M:%S", level=logging.ERROR)
     #numThreads = 96
-    total = 2*numThreads
+    total = 6*numThreads
     filepath = Path(__file__)
     idb = InstanceDatabase(
         filepath.parent.parent / "challenge_instances_cgshop25" / "zips" / "challenge_instances_cgshop25_rev1.zip")
@@ -197,7 +197,7 @@ def seeded_Multi():
         result.wait()
         allSolutions = [[sol for sol in sols] for sols in returner]
 
-        solLoc = filepath.parent.parent / "instance_solutions" / "out"
+        solLoc = filepath.parent.parent / "instance_solutions" / "576CircleArrNoOutRNoSegs"
 
         for i in range(len(allSolutions)):
             solname = "seed"+str(seeds[i])+".zip"
